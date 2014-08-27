4b76d9d12385d6adf29bc87554f590dbfc97cd49
mikoweb <rafal@mikoweb.pl>
2014-08-27 23:10:34 +0000

    widoki

--- item.php
+++ item.php
@@ -1,1 +1,17 @@
-Wpis
+<?php
+
+/**
+ * @package Joomla FlexiTwig Template
+ * @author Rafał Mikołajun <rafal@mikoweb.pl>
+ * @url http://www.vision-web.pl
+ * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+defined('_JEXEC') or die;
+defined('RAPID_FRAMEWORK') or die('Joomla! Rapid Framework is not installed.');
+
+use Joomla\Rapid\FlexiContent\Templating\ItemTwigView;
+
+echo ItemTwigView::create($this)
+    ->render()
+;
